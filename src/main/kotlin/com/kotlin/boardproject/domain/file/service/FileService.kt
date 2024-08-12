3c01bd710fe0dfe63d2f04b2c8e4e0be990238d0
mirageoasis <kimhw0820@naver.com>
2024-08-12 21:34:40 +0900

    chore: file upload test
    Signed-off-by: mirageoasis <kimhw0820@naver.com>

--- src/main/kotlin/com/kotlin/boardproject/domain/file/service/FileService.kt
+++ src/main/kotlin/com/kotlin/boardproject/domain/file/service/FileService.kt
@@ -1,10 +1,13 @@
 package com.kotlin.boardproject.domain.file.service
 
 import org.springframework.stereotype.Service
+import org.springframework.web.multipart.MultipartFile
 
 @Service
 interface FileService {
     suspend fun geneatePreSingedUrl(
         userEmail: String,
     ): String
+
+    suspend fun uploadFile(file: MultipartFile): String
 }
